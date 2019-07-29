955c4403c35289bf60226ee6079db75a71d6ae5d
Chris Young <chrisyoung@Chriss-MacBook-Pro.local>
2019-07-28 21:43:59 -0700

    autolaod commands

---
+++ lib/domain/parser/aggregate/commands.rb
@@ -0,0 +1,12 @@
+class Domain
+  class Parser
+    class Aggregate
+      class Commands
+        attr_accessor :ruby_file
+        def file_name
+          'commands.rb'
+        end
+      end
+    end
+  end
+end
