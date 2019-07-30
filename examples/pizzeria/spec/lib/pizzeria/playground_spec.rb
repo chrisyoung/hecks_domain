87fd1e0fb380975c602394bf13de9d9b87a781b6
Chris Young <chrisyoung@Chriss-MacBook-Pro.local>
2019-07-30 12:25:26 -0700

    dont generate the domain in the playground

--- examples/pizzeria/spec/lib/pizzeria/playground_spec.rb
+++ examples/pizzeria/spec/lib/pizzeria/playground_spec.rb
@@ -1,31 +1,7 @@
 require 'spec_helper'
-describe "Playground" do
-  let(:domain) do
-    Domain.domain(:Pizzeria) do
-      aggregate :Pizzas do
-        head :Pizza do
-          value(:name).as :PizzaName
-          value(:description).as :PizzaDescription
-          currency(:price)
-          list(:toppings).as :Topping
-        end
-        value(:Topping) { string :name }
-        value(:PizzaName) { string :value }
-        value(:PizzaDescription) { string :value }
-      end
-    
-      aggregate :Orders do
-        head(:Order) { list(:line_items).as :LineItem }
-        value :LineItem do
-          string :pizza_name
-          integer :quantity
-          currency :price
-          optional string :foo
-        end
-      end
-    end
-  end
+require_relative '../../../../../lib/domain'
 
+describe "Playground" do
   let(:pizza) do
     Pizzeria::Pizzas::Pizza.new(
       name: { value: 'pepperoni' }, 
@@ -51,8 +27,6 @@ describe "Playground" do
   
   describe '#create' do
     it 'saves the order' do
-      domain.build
-      domain.dump
       Pizzeria::Orders.create order
       expect(Pizzeria::Orders::Order.fetch(order.id)).to eq order
       order.line_items << Pizzeria::Orders::LineItem.from_pizza(pizza)
--- examples/pizzeria/spec/spec_helper.rb
+++ examples/pizzeria/spec/spec_helper.rb
@@ -1,2 +1,1 @@
 require_relative '../lib/pizzeria'
-require_relative '../../../lib/domain'
