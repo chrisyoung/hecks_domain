2bd48b3977ccbe9b24220355121f3f20a2360a16
Chris Young <chrisyoung@Chriss-MacBook-Pro.local>
2019-08-08 16:14:59 -0700

    clean up tests

--- examples/soccer_season/spec/playground_spec.rb
+++ examples/soccer_season/spec/playground_spec.rb
@@ -7,8 +7,8 @@ describe "Playground" do
   let(:match) do
     SoccerSeason::Matches::Match.new(
       fixture: fixture,
-      teams: [redteam, blueteam],
-      pitch: pitch,
+      teams: [@redteam, @blueteam],
+      pitch: @pitch,
       result: nil
     ).tap(&:save)
   end
@@ -21,82 +21,68 @@ describe "Playground" do
     )
   end
 
-  let(:redteam) do
-    SoccerSeason::Teams::Team.new(name: 'redteam').tap(&:save)
-  end
+  before do
+    @pitch = SoccerSeason::Pitches::Pitch.new(name: 'downtown').tap(&:save)
 
-  let(:blueteam) do
-    SoccerSeason::Teams::Team.new(name: 'blueteam').tap(&:save)
-  end
+    # Teams
+    @redteam = SoccerSeason::Teams::Team.new(name: 'redteam').tap(&:save)  
+    @blueteam = SoccerSeason::Teams::Team.new(name: 'blueteam').tap(&:save)
+    @greenteam = SoccerSeason::Teams::Team.new(name: 'greenteam').tap(&:save)
 
-  let(:pitch) do
-    SoccerSeason::Pitches::Pitch.new(name: 'downtown').tap(&:save)
-  end
+    # Players
+    @chris = SoccerSeason::Players::Player.new(name: 'chris', team: @redteam).tap(&:save)  
+    @foster = SoccerSeason::Players::Player.new(name: 'foster', team: @redteam).tap(&:save)
+    @clayton = SoccerSeason::Players::Player.new(name: 'clayton', team: @blueteam).tap(&:save)
 
-  let(:chris) do
-    SoccerSeason::Players::Player.new(name: 'chris', team: redteam).tap(&:save)
-  end
-
-  let(:foster) do
-    SoccerSeason::Players::Player.new(name: 'foster', team: redteam).tap(&:save)
-  end
-
-  let(:clayton) do
-    SoccerSeason::Players::Player.new(name: 'clayton', team: blueteam).tap(&:save)
-  end
-
-  describe 'Playground' do
-    before do
-      5.times do
-        match.add_goal!(time: Time.now, player: foster)
-      end
-
-      4.times do
-        match.add_goal!(time: Time.now, player: clayton)
-      end
+    5.times do
+      match.add_goal!(time: Time.now, player: @foster)
     end
 
-    it 'Has a head' do
-      expect(
-        SoccerSeason::Pitches::Head.superclass
-      ).to eq SoccerSeason::Pitches::Pitch
+    4.times do
+      match.add_goal!(time: Time.now, player: @clayton)
+    end
+  end
+
+  it 'Has a head' do
+    expect(
+      SoccerSeason::Pitches::Head.superclass
+    ).to eq SoccerSeason::Pitches::Pitch
+  end
+
+  it 'Events' do
+    # Events
+
+    match.score!
+    expect(match.result.winner).to eq (@redteam)
+
+    # I'm tired of calling score! every time I add a goal
+    HecksDomain::Events.subscribe(ScoreOnAddGoal.new)
+    3.times do
+      match.add_goal!(time: Time.now, player: @clayton)
+    end
+    expect(match.result.winner).to eq(@blueteam)
+  end
+
+  it 'Winner!' do
+    match.score!
+    expect(match.result.winner).to eq @redteam
+
+    3.times do
+      match.add_goal!(time: Time.now, player: @clayton)
     end
 
-    it 'Events' do
-      # Events
+    match.score!
+    expect(match.result.winner).to eq @blueteam
+  end
 
-      match.score!
-      expect(match.result.winner).to eq (redteam)
+  it 'Tied!' do
+    match.add_goal!(time: Time.now, player: @clayton)
+    match.score!
+    expect(match.result).to be_a(SoccerSeason::Matches::TiedResult)
+  end
 
-      # I'm tired of calling score! every time I add a goal
-      HecksDomain::Events.subscribe(ScoreOnAddGoal.new)
-      3.times do
-        match.add_goal!(time: Time.now, player: clayton)
-      end
-      expect(match.result.winner).to eq (blueteam)
-    end
-
-    it 'Winner!' do
-      match.score!
-      expect(match.result.winner).to eq redteam
-
-      3.times do
-        match.add_goal!(time: Time.now, player: clayton)
-      end
-
-      match.score!
-      expect(match.result.winner).to eq blueteam
-    end
-
-    it 'Tied!' do
-      match.add_goal!(time: Time.now, player: clayton)
-      match.score!
-      expect(match.result).to be_a(SoccerSeason::Matches::TiedResult)
-    end
-
-    it 'Invariants' do
-      match.teams = [redteam, blueteam, SoccerSeason::Teams::Team.new(name: 'greenteam').tap(&:save)]
-      expect { match.save }.to raise_error('Must have exactly two teams')
-    end
+  it 'Invariants' do
+    match.teams = [@redteam, @blueteam, @greenteam]
+    expect { match.save }.to raise_error('Must have exactly two teams')
   end
 end
