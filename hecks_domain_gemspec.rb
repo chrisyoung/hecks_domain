a7c9dc5762f273b4014474aaa783525b19e37959
Chris Young <chrisyoung@Chriss-MacBook-Pro.local>
2019-09-03 17:41:36 -0700

    gemspec

---
+++ hecks_domain_gemspec.rb
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name        = 'hecks_domain'
+  s.version     = '0.1.0'
+  s.licenses    = ['MIT']
+  s.summary     = '3DSL'
+  s.description = 'A Domain Specific Language for generating Domain Driven Designs'
+  s.authors     = ['Hecks Domain Developer']
+  s.email       = 'dev@hecks_domain.com'
+  s.files       = Dir['lib/**/*.rb']
+  s.homepage    = 'http://www.example.com'
+
+  s.add_runtime_dependency 'erubis'
+  s.add_runtime_dependency 'thor'
+end
