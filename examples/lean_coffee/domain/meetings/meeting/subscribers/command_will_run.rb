f6e1218f5d079b0d79bb8b486687f657a884cd5d
Chris Young <chrisyoung@Chriss-MacBook-Pro.local>
2019-09-12 12:14:27 -0700

    fix this subscriber

--- examples/lean_coffee/domain/meetings/meeting/subscribers/command_will_run.rb
+++ examples/lean_coffee/domain/meetings/meeting/subscribers/command_will_run.rb
@@ -1,20 +1,23 @@
 module LeanCoffee
-  module Meetings
-    class Meeting
-      module Subscribers
-        class CommandWillRun
-          def self.domain_event
-            LeanCoffee::Events::CommandWillRun
+  module Domain
+    module Meetings
+      class Meeting
+        module Subscribers
+          class CommandWillRun
+            def self.domain_event
+              LeanCoffee::Events::CommandWillRun
+            end
+
+            def self.notify(event)
+              # require 'pry'; binding.pry
+              return unless event.root
+              return unless event.root.class == LeanCoffee::Domain::Meetings::Meeting
+
+              Services::SimpleStateMachine.new(event.root).verify!(event.command.command_to_run)
+            end
+
+            Support::Events::DomainEventPublisher.subscribe(self)
           end
-
-          def self.notify(event)
-            return unless event.root
-            return unless event.root.class == LeanCoffee::Meetings::Meeting
-
-            Services::SimpleStateMachine.new(event.root).verify!(event.command.command_to_run)
-          end
-
-          Support::Events::DomainEventPublisher.subscribe(self)
         end
       end
     end
